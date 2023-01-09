c2ab1d16bcda92cb97244e3e8236287c176a786f
badukwei <linwgpeter@gmail.com>
2023-01-09 16:41:44 +0800

    project data-binding

--- src/app/page/member/table/table.component.ts
+++ src/app/page/member/table/table.component.ts
@@ -12,7 +12,8 @@ export class TableComponent {
   /** 是否顯示 */
   isShow = false;
   /**資料 */
-  data: any = [
+  //Author
+  authorData: any = [
     {
       author: 'John Michael',
       authorUrl: '../assets/img/team-2.jpg',
@@ -37,6 +38,51 @@ export class TableComponent {
     },
   ];
 
+  //Project
+  projectData: any = [
+    {
+      project: 'Spotify',
+      projectUrl: '../assets/img/small-logos/logo-spotify.svg',
+      budget: 2500,
+      status: 'working',
+      completion: 60,
+    },
+    {
+      project: 'Invision',
+      projectUrl: '../assets/img/small-logos/logo-invision.svg',
+      budget: 5000,
+      status: 'done',
+      completion: 100,
+    },
+    {
+      project: 'Jira',
+      projectUrl: '../assets/img/small-logos/logo-jira.svg',
+      budget: 3400,
+      status: 'canceled',
+      completion: 30,
+    },
+    {
+      project: 'Slack',
+      projectUrl: '../assets/img/small-logos/logo-slack.svg',
+      budget: 1000,
+      status: 'canceled',
+      completion: 0,
+    },
+    {
+      project: 'Webdev',
+      projectUrl: '../assets/img/small-logos/logo-webdev.svg',
+      budget: 14000,
+      status: 'working',
+      completion: 80,
+    },
+    {
+      project: 'Adobe XD',
+      projectUrl: '../assets/img/small-logos/logo-xd.svg',
+      budget: 2300,
+      status: 'done',
+      completion: 100,
+    },
+  ];
   /**
    * 變更標題
    */
