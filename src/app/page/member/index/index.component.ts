9d61ecfcda894c0e3279aff04ddba257914105ce
kimi <kimi.lai@tpisoftware.com>
2023-01-10 14:38:23 +0800

    fix: 修正首頁走勢圖顯示問題

--- src/app/page/member/index/index.component.ts
+++ src/app/page/member/index/index.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { createCharts } from './chart';
 
 /** 首頁 */
 @Component({
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss'],
 })
-export class IndexComponent {}
+export class IndexComponent implements AfterViewInit {
+  /**
+   * Angular 生命週期
+   * 響應檢視變更
+   */
+  ngAfterViewInit() {
+    let ctx1 = (document.getElementById('chart-line') as any).getContext('2d');
+    createCharts(ctx1);
+  }
+}
